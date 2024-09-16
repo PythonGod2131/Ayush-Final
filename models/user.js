649dbb6bac522b96f3a5968d00e61cfc3128c8e1
PythonGod2131 <whiteedevil3103@gmail.com>
2024-09-16 13:35:35 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Create and export user model
+module.exports = mongoose.model('User', userSchema);
